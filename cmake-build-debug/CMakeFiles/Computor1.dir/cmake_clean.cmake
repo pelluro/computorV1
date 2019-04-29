3f83f5423feedc296fe144f30edf3a8c98d3a179
Minh Pham <mipham@student.42.fr>
2019-04-29 10:41:35 +0200

    initialisation projet

---
+++ cmake-build-debug/CMakeFiles/Computor1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Computor1.dir/main.cpp.o"
+  "Computor1.pdb"
+  "Computor1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Computor1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
